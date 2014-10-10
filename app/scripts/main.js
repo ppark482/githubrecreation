2bccf4a7078ba849be4ab32da07412f71201da20
Paul Park <ppark482@gmail.com>
2014-10-10 18:30:40 -0400

    finishing up javascript

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,7 @@
 var url = 'https://api.github.com/users/ppark482';
-
+var repoURL = 'https://api.github.com/users/ppark482/repos';
+var starURL =
+'https://api.github.com/users/ppark482/starred'
 // var userName = '<%= login %>';
 //
 // var userNameR = _.template(userName);
@@ -24,16 +26,23 @@ $.getJSON(url).done( function (user_data) {
 var sidebar_template = $('#sidebar_user').html(),
     sidebar_render = _.template(sidebar_template),
     sidestat_template = $('#sidebar_stats').html(),
-    sidestat_render = _.template(sidestat_template)
+    sidestat_render = _.template(sidestat_template),
     sidefoll_template = $('#side_followers').html(),
     sidefoll_render = _.template(sidefoll_template);
 
 $.getJSON(url).done( function (user_data) {
   $('.nameBox').append(sidebar_render(user_data));
-  $('.userStats ul').append(sidestat_render(user_data));   $('.followerCounter').append(sidefoll_render(user_data));
+  $('.userStats ul').append(sidestat_render(user_data));
+  $('.followerCounter').html(sidefoll_render(user_data));
 });
 
-var repoURL = 'https://api.github.com/users/ppark482/repos';
+var star_template = $('#stargaze_count').html(),
+    star_render = _.template(star_template);
+
+$.getJSON(starURL).done( function (user_data) {
+    var starCount = user_data.length;
+    $('.counterTwo').html(starCount);
+});
 
 var repo_template = $('#allRepositories').html(),
     repo_render = _.template(repo_template);
